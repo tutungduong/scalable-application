4a09abe45fc16b9213bcd0b718fcb5972402cf56
c0mr4de <c0mr4dex@hotmail.com>
2023-09-20 16:01:00 +0700

    Update commit

---
+++ src/main/java/com/javaguides/scalable/ScalableApplication.java
@@ -0,0 +1,13 @@
+package com.javaguides.scalable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScalableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScalableApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javaguides/scalable/ScalableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaguides.scalable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScalableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
