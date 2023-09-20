e78565c426a1f0ea4f0dcd4a959e90a0ef78351e
c0mr4de <c0mr4dex@hotmail.com>
2023-09-20 23:59:24 +0700

    Update fragment and fix error

---
+++ src/main/java/com/javaguides/scalable/ScalableApplicationConfiguration.java
@@ -0,0 +1,19 @@
+package com.javaguides.scalable;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+
+@Configuration
+public class ScalableApplicationConfiguration {
+    @Bean
+    public ITemplateResolver svgTemplateResolver() {
+        SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+        resolver.setPrefix("classpath:/templates/svg/");
+        resolver.setSuffix(".svg");
+        resolver.setTemplateMode("XML");
+
+        return resolver;
+    }
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
-  content: ['./src/main/resources/templates/**/*.html'],
+  content: ['./src/main/resources/templates/**/*.html',
+    './src/main/resources/templates/**/*.svg'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    }
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms')
+  ]
 }
 
